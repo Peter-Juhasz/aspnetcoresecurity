e56bb3bd39c0805ea1c1295fd23448961836ee16
Peter-Juhasz <peter.juhasz@live.com>
2017-04-15 12:29:36 +0200

    XSS

---
+++ src/XXSSProtectionHeader/XXSSProtectionMiddleware.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Http;
+using PeterJuhasz.AspNetCore.Extensions.Security;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.AspNetCore.Builder
+{
+    public static partial class AppBuilderExtensions
+    {
+        public static void UseXXSSProtection(this IApplicationBuilder app, XXssProtectionOptions options)
+        {
+            app.UseMiddleware<XXSSProtectionMiddleware>(options);
+        }
+
+        public static void UseXXSSProtection(
+            this IApplicationBuilder app,
+            bool enabled = true,
+            bool block = true,
+            Uri reportUri = null
+        )
+        {
+            app.UseMiddleware<XXSSProtectionMiddleware>(new XXssProtectionOptions
+            {
+                Enabled = enabled,
+                Block = block,
+                ReportUri = reportUri,
+            });
+        }
+
+
+        internal sealed class XXSSProtectionMiddleware
+        {
+            public XXSSProtectionMiddleware(RequestDelegate next, XXssProtectionOptions options)
+            {
+                _next = next;
+                _headerValue = ConstructHeaderValue(options);
+            }
+
+            private readonly RequestDelegate _next;
+            private readonly string _headerValue;
+
+            internal static string ConstructHeaderValue(XXssProtectionOptions options)
+            {
+                StringBuilder builder = new StringBuilder();
+                builder.Append(options.Enabled ? "1" : "0");
+
+                if (options.Block)
+                    builder.Append("; mode=block");
+
+                if (options.ReportUri != null)
+                    builder.Append($"report={options.ReportUri}");
+
+                return builder.ToString();
+            }
+
+            public async Task Invoke(HttpContext context)
+            {
+                context.Response.OnStarting(() =>
+                {
+                    HttpResponse response = context.Response;
+
+                    if (response.GetTypedHeaders().ContentType.MediaType?.Equals("text/html", StringComparison.OrdinalIgnoreCase) ?? false)
+                    {
+                        response.Headers["X-XSS-Protection"] = _headerValue;
+                    }
+
+                    return Task.CompletedTask;
+                });
+
+                await _next.Invoke(context);
+            }
+        }
+    }
+}
---
+++ src/XXSSProtectionHeader/XXssProtectionOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PeterJuhasz.AspNetCore.Extensions.Security
+{
+    public class XXssProtectionOptions
+    {
+        public bool Enabled { get; set; } = true;
+
+        public bool Block { get; set; } = true;
+
+        public Uri ReportUri { get; set; }
+    }
+}
