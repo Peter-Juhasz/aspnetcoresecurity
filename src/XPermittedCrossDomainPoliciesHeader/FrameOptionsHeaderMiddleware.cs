58700399965d81c1d3f43dabc0128da7bdaa3093
Peter-Juhasz <peter.juhasz@live.com>
2017-04-15 16:15:33 +0200

    X-Permitted-Cross-Domain-Policies

---
+++ src/XPermittedCrossDomainPoliciesHeader/FrameOptionsHeaderMiddleware.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using PeterJuhasz.AspNetCore.Extensions.Security;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Microsoft.AspNetCore.Builder
+{
+    public static partial class AppBuilderExtensions
+    {
+        /// <summary>
+        /// Adds X-Permitted-Cross-Domain-Policies header to all responses.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="policy"></param>
+        public static void UseXPermittedCrossDomainPoliciesHeader(this IApplicationBuilder app, PermittedCrossDomainPolicy policy = PermittedCrossDomainPolicy.None)
+        {
+            app.UseMiddleware<XPermittedCrossDomainPoliciesMiddleware>(policy);
+        }
+
+
+        internal sealed class XPermittedCrossDomainPoliciesMiddleware
+        {
+            public XPermittedCrossDomainPoliciesMiddleware(RequestDelegate next, PermittedCrossDomainPolicy policy)
+            {
+                _next = next;
+                Policy = policy;
+                _headerValue = HeaderValues[policy];
+            }
+
+            private readonly RequestDelegate _next;
+            private readonly string _headerValue;
+
+            private static readonly IReadOnlyDictionary<PermittedCrossDomainPolicy, string> HeaderValues = new Dictionary<PermittedCrossDomainPolicy, string>
+            {
+                { PermittedCrossDomainPolicy.None, "none" },
+                { PermittedCrossDomainPolicy.MasterOnly, "master-only" },
+                { PermittedCrossDomainPolicy.ByContentType, "by-content-type" },
+                { PermittedCrossDomainPolicy.ByFtpFileName, "by-ftp-filename" },
+                { PermittedCrossDomainPolicy.All, "all" },
+            };
+
+            public PermittedCrossDomainPolicy Policy { get; }
+
+            public async Task Invoke(HttpContext context)
+            {
+                context.Response.OnStarting(() =>
+                {
+                    HttpResponse response = context.Response;
+                    response.Headers["X-Permitted-Cross-Domain-Policies"] = _headerValue;
+
+                    return Task.CompletedTask;
+                });
+
+                await _next.Invoke(context);
+            }
+        }
+    }
+}
---
+++ src/XPermittedCrossDomainPoliciesHeader/PermittedCrossDomainPolicy.cs
@@ -0,0 +1,30 @@
+﻿namespace PeterJuhasz.AspNetCore.Extensions.Security
+{
+    public enum PermittedCrossDomainPolicy
+    {
+        /// <summary>
+        /// No policy files are allowed anywhere on the target server, including this master policy file. 
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// Only this master policy file is allowed. 
+        /// </summary>
+        MasterOnly,
+
+        /// <summary>
+        /// Only policy files served with Content-Type: text/x-cross-domain-policy are allowed. 
+        /// </summary>
+        ByContentType,
+
+        /// <summary>
+        /// Only policy files whose file names are crossdomain.xml (i.e. URLs ending in /crossdomain.xml) are allowed.
+        /// </summary>
+        ByFtpFileName,
+
+        /// <summary>
+        /// All policy files on this target domain are allowed. 
+        /// </summary>
+        All,
+    }
+}
