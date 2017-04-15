1958d8a7a2cb6fbe6db87a99134609f31423c723
Peter-Juhasz <peter.juhasz@live.com>
2017-04-15 19:05:58 +0200

    X-Content-Type-Options

---
+++ src/XContentTypeOptionsHeader/XContentTypeOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace PeterJuhasz.AspNetCore.Extensions.Security
+{
+    public enum XContentTypeOptions
+    {
+        /// <summary>
+        /// The script and styleSheet elements will reject responses with incorrect MIME types.
+        /// </summary>
+        NoSniff,
+    }
+}
---
+++ src/XContentTypeOptionsHeader/XContentTypeOptionsHeaderMiddleware.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using PeterJuhasz.AspNetCore.Extensions.Security;
+using System.Threading.Tasks;
+
+namespace Microsoft.AspNetCore.Builder
+{
+    public static partial class AppBuilderExtensions
+    {
+        /// <summary>
+        /// Adds the X-Content-Type-Options header to all responses.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="policy"></param>
+        public static void UseXContentTypeOptionsHeader(this IApplicationBuilder app, XContentTypeOptions policy = XContentTypeOptions.NoSniff)
+        {
+            app.UseMiddleware<XContentTypeOptionsMiddleware>();
+        }
+
+
+        internal sealed class XContentTypeOptionsMiddleware
+        {
+            public XContentTypeOptionsMiddleware(RequestDelegate next)
+            {
+                _next = next;
+            }
+
+            private readonly RequestDelegate _next;
+
+            public async Task Invoke(HttpContext context)
+            {
+                context.Response.OnStarting(() =>
+                {
+                    HttpResponse response = context.Response;
+                    response.Headers["X-Content-Type-Options"] = "nosniff";
+
+                    return Task.CompletedTask;
+                });
+
+                await _next.Invoke(context);
+            }
+        }
+    }
+}
