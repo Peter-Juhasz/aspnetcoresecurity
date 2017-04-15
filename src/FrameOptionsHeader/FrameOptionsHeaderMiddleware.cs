4aa8d1692bb8d3f7ca9bcf2e85a477a96ff08a0c
Peter-Juhasz <peter.juhasz@live.com>
2017-04-15 15:39:45 +0200

    docs

--- src/FrameOptionsHeader/FrameOptionsHeaderMiddleware.cs
+++ src/FrameOptionsHeader/FrameOptionsHeaderMiddleware.cs
@@ -7,11 +7,21 @@ namespace Microsoft.AspNetCore.Builder
 {
     public static partial class AppBuilderExtensions
     {
+        /// <summary>
+        /// Adds the Frame-Options and X-Frame-Options headers to responses with content type text/html.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="options"></param>
         public static void UseFrameOptionsHeader(this IApplicationBuilder app, FrameOptionsOptions options)
         {
             app.UseMiddleware<FrameOptionsMiddleware>(options);
         }
 
+        /// <summary>
+        /// Adds the Frame-Options and X-Frame-Options headers to responses with content type text/html.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="configure"></param>
         public static void UseFrameOptionsHeader(this IApplicationBuilder app, Action<FrameOptionsOptions> configure)
         {
             if (configure == null)
@@ -22,6 +32,11 @@ namespace Microsoft.AspNetCore.Builder
             app.UseFrameOptionsHeader(options);
         }
 
+        /// <summary>
+        /// Adds the Frame-Options and X-Frame-Options headers to responses with content type text/html.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="policy"></param>
         public static void UseFrameOptionsHeader(this IApplicationBuilder app, FrameOptionsPolicy policy = FrameOptionsPolicy.Deny)
         {
             if (policy == FrameOptionsPolicy.AllowFrom)
@@ -30,6 +45,11 @@ namespace Microsoft.AspNetCore.Builder
             app.UseFrameOptionsHeader(new FrameOptionsOptions(policy));
         }
 
+        /// <summary>
+        /// Adds the Frame-Options and X-Frame-Options headers to responses with content type text/html.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="allowFromUri"></param>
         public static void UseFrameOptionsHeader(this IApplicationBuilder app, Uri allowFromUri)
         {
             app.UseFrameOptionsHeader(new FrameOptionsOptions(allowFromUri));
--- src/FrameOptionsHeader/FrameOptionsPolicy.cs
+++ src/FrameOptionsHeader/FrameOptionsPolicy.cs
@@ -1,9 +1,23 @@
 ﻿namespace PeterJuhasz.AspNetCore.Extensions.Security
 {
+    /// <summary>
+    /// Defines the Frame Options policy.
+    /// </summary>
     public enum FrameOptionsPolicy
     {
+        /// <summary>
+        /// The page cannot be displayed in a frame, regardless of the site attempting to do so.
+        /// </summary>
         Deny = 0,
+
+        /// <summary>
+        /// The page can only be displayed in a frame on the same origin as the page itself.
+        /// </summary>
         SameOrigin,
+
+        /// <summary>
+        /// The page can only be displayed in a frame on the specified origin.
+        /// </summary>
         AllowFrom,
     }
 }
