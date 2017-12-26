ba7ecdbd12b9ee27eebd6cd4ba420818ae7c3dea
Juhász Péter <peter.juhasz@live.com>
2017-12-26 08:40:38 +0100

    Expect-CT

---
+++ src/ExpectCT/ExpectCTMiddleware.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Http;
+using PeterJuhasz.AspNetCore.Extensions.Security;
+using System;
+using System.Threading.Tasks;
+
+namespace Microsoft.AspNetCore.Builder
+{
+    public static partial class AppBuilderExtensions
+    {
+        /// <summary>
+        /// Adds the Expect-CT header which allows sites to opt in to reporting and/or enforcement of Certificate Transparency requirements.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="options"></param>
+        public static void UseExpectCT(this IApplicationBuilder app, ExpectCTOptions options)
+        {
+            app.UseMiddleware<ExpectCTMiddleware>(options);
+        }
+
+        /// <summary>
+        /// Adds the Expect-CT header which allows sites to opt in to reporting and/or enforcement of Certificate Transparency requirements.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="enforce">Enforce Certificate Transparency.</param>
+        /// <param name="maxAge">Specifies the number of seconds that the browser should cache and apply the received policy.</param>
+        /// <param name="reportUri">Sets the URI the browser is going to report violations to.</param>
+        public static void UseExpectCT(
+            this IApplicationBuilder app,
+            bool enforce = true,
+            TimeSpan? maxAge = null,
+            Uri reportUri = null
+        )
+        {
+            app.UseExpectCT(new ExpectCTOptions
+            {
+                Enforce = enforce,
+                MaxAge = maxAge,
+                ReportUri = reportUri,
+            });
+        }
+
+
+        internal sealed class ExpectCTMiddleware
+        {
+            public ExpectCTMiddleware(RequestDelegate next, ExpectCTOptions options)
+            {
+                _next = next;
+                Options = options ?? throw new ArgumentNullException(nameof(options));
+                _headerValue = Options.ToString();
+            }
+
+            private readonly RequestDelegate _next;
+            private readonly string _headerValue;
+
+            public ExpectCTOptions Options { get; }
+            
+            public async Task Invoke(HttpContext context)
+            {
+                context.Response.OnStarting(() =>
+                {
+                    HttpResponse response = context.Response;
+                    response.Headers["Expect-CT"] = _headerValue;
+                    return Task.CompletedTask;
+                });
+
+                await _next.Invoke(context);
+            }
+        }
+    }
+}
---
+++ src/ExpectCT/ExpectCTOptions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PeterJuhasz.AspNetCore.Extensions.Security
+{
+    public class ExpectCTOptions
+    {
+        /// <summary>
+        /// Gets or sets whether enforce policy or keep it in report-only mode.
+        /// </summary>
+        public bool Enforce { get; set; } = true;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public TimeSpan? MaxAge { get; set; } = null;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Uri ReportUri { get; set; }
+
+
+        public override string ToString()
+        {
+            IList<string> directives = new List<string>();
+            
+            if (this.Enforce)
+                directives.Add("enforce");
+
+            if (this.MaxAge != null)
+                directives.Add($"max-age={(int)this.MaxAge.Value.TotalSeconds}");
+
+            if (this.ReportUri != null)
+                directives.Add($"report-uri={this.ReportUri}");
+           
+            return String.Join(", ", directives);
+        }
+    }
+}
